7212e1137238da731646e51c2ede646e19d1c899
Moustakime KIFIA <mkifia@icloud.com>
2016-01-28 17:13:20 +0100

    Controllers

---
+++ src/AppBundle/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController extends Controller
+{
+	/**
+	 *
+	 * @Route("/hello/{name}", name="hello")
+	 *
+	 */
+	
+	public function indexAction($name) {
+		return $this->render('hello/index.html.twig');
+	}
+}
